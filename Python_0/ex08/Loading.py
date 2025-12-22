8b5aaf641ba1599d57996a722886bd4a1b637a05
Pierre-Loup Scala <pscala@f0r4s20.clusters.42paris.fr>
2025-12-22 18:37:46 +0100

    jojolion

--- Python_0/ex08/Loading.py
+++ Python_0/ex08/Loading.py
@@ -1,9 +1,29 @@
 import os
-from colors import YELLOW, BOLD, RESET
+from colors import YELLOW, BLUE, RESET
+
 
 def ft_tqdm(lst: range) -> None:
 
-	"""Wrap a range and yield its values,
-	printing a tqdm-like progress bar."""
+    """Wrap a range and yield its values,
+    printing a tqdm-like progress bar."""
 
+    cols = os.get_terminal_size().columns
+    total = (len(lst))
+    marge = (len(str(lst)) * 2 + 14)
+    bar_len = (cols - marge) - 3
 
+    if total < 1:
+        print("0it [00:00, ?it/s]")
+        return
+
+    for idx, elem in enumerate(lst, start=0):
+        filled = max(0, int(bar_len * idx / len(lst)))
+        bar = "=" * filled + ">" + " " * max(0, (bar_len - filled))
+        percent = int(100 * idx / total)
+        line = f"{BLUE}{percent:3d}%|[{YELLOW}{bar}{BLUE}]| {idx}/{total}"
+        print("\r" + line + "\033[K", end="", flush=True)
+        yield elem
+
+    bar = "=" * bar_len
+    line = f"{BLUE}{100:3d}%|[{YELLOW}{bar}>{BLUE}]| {total}/{total}"
+    print("\r" + line + "\033[K" + RESET, end="", flush=True)
--- Python_0/ex08/tester.py
+++ Python_0/ex08/tester.py
@@ -1,12 +1,11 @@
 from time import sleep
 from tqdm import tqdm
-import time
+from Loading import ft_tqdm
 
-# for elem in ft_tqdm(range(333)):
-# 	sleep(0.005)
-# print()
-for elem in tqdm(range(0, 333)):
-	# sleep(0.005)
-	sleep(0.5)
 
-# print(tqdm.__doc__)
+for elem in ft_tqdm(range(333)):
+    sleep(0.005)
+print()
+for elem in tqdm(range(333)):
+    sleep(0.005)
+print()
